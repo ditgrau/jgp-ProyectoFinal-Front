5c804365d9437c25e5e9884ae562c3b970791d4a
DitGrau <juditgraup@gmail.com>
2023-07-17 19:07:47 +0200

    feat: navbar basica de bootstrap, fixed bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Body } from './pages/Body/Body.jsx'
+import { Navigation } from './common/Navigation/Navigation'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Hola, Alicia</h1>
-        <h2>Calendario</h2>
-        <h3>Nombre:</h3><span>soy alicia</span>  
-      </div>
+      <Body/>
+      <Navigation/>
     </>
   )
 }
---
+++ src/common/Navigation/Navigation.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import './Navigation.css'
+import { Navbar, Container, Nav, Offcanvas } from "react-bootstrap"
+
+///////////////////////////////////
+
+export function Navigation() {
+    return (
+        <>
+                <Navbar key='lg' expand='lg' className="bg-body-tertiary mb-3" fixed="bottom">
+                    <Container fluid>
+                        <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-lg`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                                    Offcanvas
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="#action1">Home</Nav.Link>
+                                    <Nav.Link href="#action2">Link</Nav.Link>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+        </>
+    )
+}
