f80e38fd80b225ec59738c181b71d81e83a63a92
DitGrau <juditgraup@gmail.com>
2023-07-23 11:30:38 +0200

    "feat: control de rutas segun role y token"

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Login } from "../Auth/Login";
 import { Register } from "../Auth/Register";
 import { Home } from "../Home/Home";
@@ -9,20 +9,36 @@ import { Results } from "../Results/Results";
 import { Control } from "../Control/Control";
 import { Users } from "../Users/Users";
 import { Credentials } from "../Credentials/Credentials";
+import { useAuth } from '../../hooks/useAuth';
 
 export function Body() {
-    return (
+    const { token, role } = useAuth();
+    const [view, setView] = useState('');
 
+    useEffect(() => {
+        const changeView = !token
+            ? '/login'
+            : role === 1
+                ? '/control'
+                : role === 3
+                    ? '/home'
+                    : '/login';
+        setView(changeView);
+    }, [token, role]);
+
+    return (
         <Routes className='bodyRoutes'>
-            <Route path="/" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/control" element={<Control/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-            <Route path="/calendar" element={<Calendar/>}/>
-            <Route path="/results" element={<Results/>}/>
-            <Route path="/users" element={<Users/>}/>
-            <Route path="/credentials" element={<Credentials/>}/>
+            <Route path="*" element={<Navigate to={view} />} />
+            <Route path="/" element={<Navigate to={view} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/control" element={<Control />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/credentials" element={<Credentials />} />
         </Routes>
 
     )
