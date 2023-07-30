813b4fb896a491378b7326903d36930e1a8d4ded
DitGrau <juditgraup@gmail.com>
2023-07-30 17:17:09 +0200

    "fix: añadido /api a endopoint"

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const ENDPOINT = "https://jgp-proyecto-final-back-bybj.vercel.app/"
+const ENDPOINT = "https://jgp-proyecto-final-back-bybj.vercel.app/api"
 const config = {
     headers: {
         "Content-Type": "application/json",
