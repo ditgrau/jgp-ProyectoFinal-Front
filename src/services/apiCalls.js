68a951dea9bb5940e5a0259c537c60bdddc77cf6
DitGrau <juditgraup@gmail.com>
2023-07-31 09:20:57 +0200

    "con doble api"

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const ENDPOINT = "https://jgp-proyecto-final-back.vercel.app/api"
+const ENDPOINT = "https://jgp-proyecto-final-back.vercel.app/api/api"
 const config = {
     headers: {
         "Content-Type": "application/json",
